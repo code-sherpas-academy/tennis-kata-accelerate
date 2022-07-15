1152bc2aab21f00414eed0a3286df17d0de143ba
David Gracia <david.gracia@code-sherpas.rocks>
2022-07-15 10:02:01 +0200

    change

--- app/src/test/kotlin/tennis/kata/accelerate/ScoringSystemTest.kt
+++ app/src/test/kotlin/tennis/kata/accelerate/ScoringSystemTest.kt
@@ -1,7 +1,30 @@
 package tennis.kata.accelerate
 
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
 internal class ScoringSystemTest {
 
+    @Test
+    fun `0-0 is LOVE-LOVE`() {
+        Assertions.assertThat(ScoringSystem(0, 0).currentScore()).isEqualTo("LOVE:LOVE")
+    }
 
+    @Test
+    fun `0-0 then 15-LOVE`() {
+        val scoringSystem = ScoringSystem(0, 0)
 
+        scoringSystem.winnerForBall(Player.ONE)
+
+        Assertions.assertThat(scoringSystem.currentScore()).isEqualTo("15:LOVE")
+    }
+
+    @Test
+    fun `0-0 then LOVE-15`() {
+        val scoringSystem = ScoringSystem(0, 0)
+
+        scoringSystem.winnerForBall(Player.TWO)
+
+        Assertions.assertThat(scoringSystem.currentScore()).isEqualTo("LOVE:15")
+    }
 }
